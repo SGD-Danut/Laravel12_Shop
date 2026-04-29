7e977116d30327ea0c25ad13551fb97c847bd671
Danut <sgd.danut@gmail.com>
2026-04-29 14:45:38 +0300

    Filtrarea produselor dupa sectiuni si categorii
    În partea de administrare a aplicatiei, unde avem tabelul cu toate produsele le putem filtra după secțiuni și categorii.

--- app/Livewire/Admin/Products.php
+++ app/Livewire/Admin/Products.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Content\Category;
 use App\Models\Content\Product;
+use App\Models\Content\Section;
 use Livewire\Component;
 use Livewire\WithPagination; // Specificăm că folosim paginația
 
@@ -13,8 +15,15 @@ class Products extends Component
     protected $paginationTheme = 'bootstrap'; // Specificăm că folosim paginația cu Bootstrap
 
     private $products = null;
+    private $sections;
     public $currentProduct;
     public $productCategories = [];
+    public $selectedSectionId = null;
+    public $selectedCategories = null;
+    public $selectedCategoryId = null;
+
+    public $selectedSectionTitle = null;
+    public $selectedCategoryTitle = null;
 
     public function obtainProductId($productId) {
         if (!$productId) {
@@ -46,11 +55,48 @@ class Products extends Component
         ]);
     }
 
+    public function selectSection($sectionId) {
+        if ($this->selectedSectionId == $sectionId) {
+            $this->selectedSectionId = null;
+            $this->selectedCategories = null;
+            $this->selectedSectionTitle = null;
+        } else {
+            $this->selectedSectionId = $sectionId;
+            $this->selectedCategories = Section::findOrFail($sectionId)->categories->sortBy('name');
+            $this->selectedSectionTitle = Section::findOrFail($sectionId)->name;
+        }
+
+        $this->selectedCategoryId = null; // Resetăm categoria dacă schimbăm secțiunea
+        // $this->resetPage(); // Trecem la pagina 1
+    }
+
+    public function selectCategory($categoryId) {
+        if ($this->selectedCategoryId == $categoryId) {
+            $this->selectedCategoryId = null;
+            $this->selectedCategoryTitle = null;
+        } else {
+            $this->selectedCategoryId = $categoryId;
+            $this->selectedCategoryTitle = Category::findOrFail($categoryId)->name;
+        }
+
+        $this->resetPage(); // Trecem la pagina 1
+    }
+
     public function render()
     {
-        $this->products = Product::query()->orderBy('created_at', 'desc')->paginate();
+        if ($this->selectedSectionId == null) {
+            $this->products = Product::query()->orderBy('created_at', 'desc')->paginate();
+        } else if ($this->selectedSectionId != null && $this->selectedCategoryId == null) {
+            $this->products = Product::query()->where('section_id', '=', $this->selectedSectionId)->orderBy('created_at', 'desc')->paginate();
+        } else if ($this->selectedSectionId != null && $this->selectedCategoryId != null) {
+            $category = Category::findOrFail($this->selectedCategoryId);
+            $this->products = $category->products()->orderBy('created_at', 'desc')->paginate();
+        }
+        
+        $this->sections = Section::all()->sortBy('position');
         return view('livewire.admin.products', [
-            'products' => $this->products
+            'products' => $this->products,
+            'sections' => $this->sections
         ]);
     }
 }
--- resources/views/livewire/admin/products.blade.php
+++ resources/views/livewire/admin/products.blade.php
@@ -12,7 +12,23 @@
     <!-- Products Table -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">List of products - {{ $products->total() }}</h6>
+            <h6 class="m-0 font-weight-bold text-primary">{{ $products->total() }} products {{ isset($selectedSectionTitle) ? 'from section:' . ' ' . $selectedSectionTitle : '' }} {{ isset($selectedCategoryTitle) ? 'and from category:' . ' ' . $selectedCategoryTitle : '' }}</h6>
+            <br>
+            <h6>Sections:</h6>
+            @forelse ($sections as $section)
+                <span wire:click="selectSection({{ $section->id }})" class="badge {{ $selectedSectionId == $section->id ? 'badge-primary' : 'badge-secondary' }}" style="cursor: pointer">{{ $section->name }}</span>
+            @empty
+                <p>No sections and no products!</p>
+            @endforelse
+            <br>
+            <h6>Categories:</h6>
+            @if (isset($selectedCategories))
+                @forelse ($selectedCategories as $selectedCategory)
+                    <span wire:click="selectCategory({{ $selectedCategory->id }})" class="badge {{ $selectedCategoryId == $selectedCategory->id ? 'badge-primary' : 'badge-secondary' }}" style="cursor: pointer">{{ $selectedCategory->name }}</span>
+                @empty
+                    <p>No selected section, no categories!</p>
+                @endforelse
+            @endif
         </div>
         @if (isset($products))
             <div class="card-body">
@@ -22,13 +38,14 @@
                             <tr>
                                 <th>#</th>
                                 <th>Name</th>
+                                <th>Section / Categories</th>
                                 <th>Image</th>
                                 <th>Price / Discount</th>
                                 <th>Views</th>
                                 <th>Stock</th>
                                 <th>Position</th>
                                 <th>Added date</th>
-                                <th>Active / Promoted</th>
+                                <th>Visibility / State</th>
                                 <th>Actions</th>
                             </tr>
                         </thead>
@@ -36,13 +53,14 @@
                             <tr>
                                 <th>#</th>
                                 <th>Name</th>
+                                <th>Section / Categories</th>
                                 <th>Image</th>
                                 <th>Price / Discount</th>
                                 <th>Views</th>
                                 <th>Stock</th>
                                 <th>Position</th>
                                 <th>Added date</th>
-                                <th>Active / Promoted</th>
+                                <th>Visibility / State</th>
                                 <th>Actions</th>
                             </tr>
                         </tfoot>
@@ -50,17 +68,52 @@
                             @forelse ($products as $product)
                             <tr>
                                 <td>{{ $products->currentPage() > 1 ? $loop->iteration + $products->perPage() * ($products->currentPage() - 1) : $loop->iteration }}</td>
-                                <td>{{ $product->name }}</td>
-                                <td class="text-center" wire:ignore>
-                                    @livewire('admin.product-image', ['model' => $product, 'defaultProductImage' => 'product.png', 'productImageDirectoryForSaving' => 'products'], key($product->id))
+                                <td>
+                                    {{ $product->name }}
+                                </td>
+                                <td>
+                                    Section: 
+                                    <span wire:click="selectSection({{ $section->id }})" class="badge {{ $selectedSectionId == $section->id ? 'badge-primary' : 'badge-secondary' }}" style="cursor: pointer">{{ $product->section->name }}</span>
+                                    <br>
+                                    Categories:
+                                    @forelse ($product->categories as $productCategory)
+                                        <span wire:click="selectCategory({{ $productCategory->id }})" class="badge {{ $selectedCategoryId == $productCategory->id ? 'badge-primary' : 'badge-secondary' }}" style="cursor: pointer">{{ $productCategory->name }}</span>
+                                    @empty
+                                        <p>This product has no categories!</p>
+                                    @endforelse
+                                </td>
+                                {{-- <td class="text-center" wire:ignore>
+                                    @livewire('admin.product-image', ['model' => $product, 'defaultProductImage' => 'product.png', 'productImageDirectoryForSaving' => 'products'])
+                                </td> --}}
+                                <td>
+                                    @if ($product->image == 'product.png')
+                                        <img src="{{ $product->defaultImageUrl() }}" width="60" alt="Product image">
+                                    @else
+                                        <img src="{{ $product->imageUrl() }}" width="60" alt="Product image">
+                                    @endif
                                 </td>
                                 <td>{{ $product->price }} / {{ $product->discount }}</td>
                                 <td>{{ $product->views }}</td>
                                 <td>{{ $product->stock }}</td>
                                 <td>{{ $product->position }}</td>
                                 <td>{{ $product->created_at->format('d.m.Y') }}</td>
-                                <td wire:ignore>
-                                    @livewire('admin.section-status',['section' => $product], key($product->id))
+                                {{-- <td wire:ignore>
+                                    @livewire('admin.section-status',['section' => $product])
+                                </td> --}}
+                                <td>  
+                                    @if ($product->active)
+                                        Active
+                                    @else
+                                        Inactive
+                                    @endif
+
+                                    /
+
+                                    @if ($product->active)
+                                        Promoted
+                                    @else
+                                        Standard
+                                    @endif
                                 </td>
                                 <td>
                                     {{-- Edit product button: --}}
@@ -72,7 +125,7 @@
                                         <i class="far fa-images"></i>
                                     </a>
                                     <!-- Button trigger for change categories modal -->
-                                    <button wire:click="obtainProductId({{ $product->id }})" type="button" class="btn btn-secondary btn-circle" data-toggle="modal" data-target="#productCategoriesModal">
+                                    <button wire:click="obtainProductId({{ $product->id }})" title="Edit product categories" type="button" class="btn btn-secondary btn-circle" data-toggle="modal" data-target="#productCategoriesModal">
                                         <i class="fas fa-bars"></i>
                                     </button>
                                 </td>
